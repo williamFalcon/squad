aae0450768b140ac73afe6478803a8a00dee3106
William Falcon <waf2107@columbia.edu>
2019-11-13 14:18:26 -0500

    Update layers.py

--- layers.py
+++ layers.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from reading_hw.util import masked_softmax
+from squad.util import masked_softmax
 
 
 class Embedding(nn.Module):
